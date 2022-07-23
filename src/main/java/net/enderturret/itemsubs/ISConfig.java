e144fb76a78d5ffe16bc257d9a38ffcb6a85571b
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-07-23 19:46:11 -0400

    Add comments to the config options.
    Honestly cannot believe I forgot these.

--- src/main/java/net/enderturret/itemsubs/ISConfig.java
+++ src/main/java/net/enderturret/itemsubs/ISConfig.java
@@ -19,8 +19,15 @@ public class ISConfig {
 	private final IntValue distanceFromFuel;
 
 	private ISConfig(ForgeConfigSpec.Builder builder) {
-		realismMode = builder.define("realismMode", true);
-		distanceFromFuel = builder.defineInRange("distanceFromFuel", 5, 1, Integer.MAX_VALUE);
+		realismMode = builder
+				.comment("Whether \"realism mode\" is enabled.",
+						"This currently affects whether you can place submarines in the air.",
+						"This feature, while fun, isn't realistic and may be undesirable in most modded environments.")
+				.define("realismMode", true);
+		distanceFromFuel = builder
+				.comment("The number of blocks a submarine will travel per item smeltable from fuel.",
+						"For example, a piece of coal smelts 8 items, so a submarine can move 40 (8 * 5) blocks with one (assuming a distanceFromFuel value of 5.)")
+				.defineInRange("distanceFromFuel", 5, 1, Integer.MAX_VALUE);
 	}
 
 	public boolean realismMode() {
