535a9f6eb314e0b753a31a2dd4eb8e55ffa5b896
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-07-23 21:39:29 -0400

    Fix submarines not rotating when damaged.

--- src/main/java/net/enderturret/itemsubs/client/entity/SubmarineRenderer.java
+++ src/main/java/net/enderturret/itemsubs/client/entity/SubmarineRenderer.java
@@ -2,6 +2,7 @@ package net.enderturret.itemsubs.client.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
 
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -9,6 +10,7 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 
 import net.enderturret.itemsubs.ItemSubs;
 import net.enderturret.itemsubs.entity.SubmarineEntity;
@@ -28,11 +30,24 @@ public class SubmarineRenderer extends EntityRenderer<SubmarineEntity> {
 	public void render(SubmarineEntity entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
 		super.render(entity, entityYaw, partialTick, poseStack, buffer, packedLight);
 
+		poseStack.pushPose();
+
+		final float hurtTime = entity.getHurtTime() - partialTick;
+
+		float damage = entity.getDamage() - partialTick;
+		if (damage < 0F)
+			damage = 0F;
+
+		if (hurtTime > 0F)
+			poseStack.mulPose(Vector3f.XP.rotationDegrees(Mth.sin(hurtTime) * hurtTime * damage / 10F * entity.getHurtDir()));
+
 		model.setupAnim(entity, 0, 0, 0, entity.getYRot(), entity.getXRot());
 
 		final VertexConsumer consumer = buffer.getBuffer(RenderType.entityCutoutNoCull(getTextureLocation(entity)));
 
 		model.renderToBuffer(poseStack, consumer, packedLight, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
+
+		poseStack.popPose();
 	}
 
 	@Override
